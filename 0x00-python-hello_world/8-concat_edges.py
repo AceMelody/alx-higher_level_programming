c48066ae2e2d28b0aa6005c5a0119ab459b34181
AceMelody <omotranyango@gmail.com>
2023-11-27 05:19:10 -0800

    python scripts

---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming\
+        language that combines remarkable power with very clear syntax"
+str = " ".join(str.split(" ")[5:7]) + " " + "".join(str.split(" ")[-4]) + " "\
+        + "".join(str.split(" ")[0])
+print(str)
