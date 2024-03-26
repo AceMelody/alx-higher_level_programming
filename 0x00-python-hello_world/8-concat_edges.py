11e15043eebdc179a24cc487b7b7480015bc3adb
AceMelody <omotranyango@gmail.com>
2024-03-26 07:02:18 -0700

    Hello Python

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
         language that combines remarkable power with very clear syntax"
-str = f"{' '.join(str.split()[5:7])} {''.join(str.split()[12:13])} {''.join(str.split()[0:1])}"
+str = ' '.join([' '.join(str.split()[5:7]), ''.join(str.split()[12:13]), ''.join(str.split()[0:1])])
 print(str)
