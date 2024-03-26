aa9371cfcf1b2c60194ea510244f116b1b7a8b72
AceMelody <omotranyango@gmail.com>
2024-03-26 10:56:06 -0700

    Hello Python

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -11,12 +11,9 @@ int check_cycle(listint_t *list)
 {
 	listint_t *temp;
 
-	if (list == NULL)
-		return (0);
 	temp = list;
-	do {
+	while (temp != NULL || temp->next != list)
 		temp = temp->next;
-	} while (temp != NULL || temp->next != list);
 	if (temp == NULL)
 		return (0);
 	if (temp == list)
