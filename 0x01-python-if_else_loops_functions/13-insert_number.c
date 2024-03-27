e93c29b0e2f3f096a0cb717c34d129227b54ce64
AceMelody <omotranyango@gmail.com>
2024-03-27 08:27:12 -0700

    Python-if else, l9ops, functions

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -12,11 +12,18 @@ listint_t *insert_node(listint_t **head, int number)
 {
 	listint_t *temp,*n_node;
 
+	if (*head == NULL)
+		return (NULL);
 	temp = *head;
 	n_node = (listint_t *) malloc(sizeof(listint_t));
 	if (n_node == NULL)
 		return (NULL);
 	n_node->n = number;
+	if (number < temp->n)
+	{
+		n_node->next = *head;
+		**head = &n_node;
+	}
 	while (temp->next->n < number)
 		temp = temp->next;
 	n_node->next = temp->next;
