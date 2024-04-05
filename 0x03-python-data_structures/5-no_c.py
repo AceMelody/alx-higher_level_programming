3f6cae98034e5f3e1b3feebd4f4390f9ba2d973d
AceMelody <omotranyango@gmail.com>
2024-04-05 05:40:28 -0700

    Python - Data structures

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
     for i in range(0, len(my_string)):
-        if my_string[i] == 'C' or my_string[i] == 'c':
+        if my_string[i] == '99' or my_string[i] == '67':
             continue
         else:
             print("{:c}".format(mystring[i]), end='')
